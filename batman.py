a7f6d45fe62426674cd33718429dea4a9ab83f34
unknown <mandenno228@gmail.com>
2021-06-23 22:52:43 +0300

    update

---
+++ batman.py
@@ -0,0 +1,14 @@
+#Black Sheep Song
+
+print ('Introduction')
+output = 'Baa Baa Black Sheep'
+
+
+
+print ('Lyrics:')
+
+print ( "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. \n \nIt has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." )
+print ()
+#Example 2
+
+print (' With 2,500$, I can buy: \n   i)  Chocolate \n   ii) Bananas \n   iii)Peas \n   iv)Bread \n   v)Milk \nAt Carrefour' )

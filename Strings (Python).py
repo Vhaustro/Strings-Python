d6633d79b400bc68a184d0b8b7f29ec981653baf
unknown <mandenno228@gmail.com>
2021-06-23 10:43:33 +0300

    update

---
+++ Strings (Python).py
@@ -0,0 +1,52 @@
+#Strings
+
+
+first_name = 'Dennis \n'
+print ()
+last_name = 'Micheal'
+#String Here !
+print (first_name + last_name)
+
+#To combine strings we use the (+) sign so as to combine them together. This is'String Concatenation'.
+
+sentence = 'The quick brown fox jumps over the lazy dog.'
+print(sentence.upper())
+print(sentence.lower())
+print(sentence.capitalize())
+print(sentence.count('a'))
+
+
+#Input of First and Last name variables to make strings
+print(input(first_name + last_name))
+print (input('What is your first Name?'))
+print (input ('How are you?'))
+print (input('What is your last name?'))
+
+print ( 'Bonjour ' + first_name.capitalize() + ' ' +last_name.capitalize())
+
+school_name ='Strathmore'
+
+print (input(school_name))
+print (input('Where do you go to school?  ' + first_name + ' ' + last_name))
+
+
+#Format Strings
+# {} is used to specify the parameters
+# E.G. [output = 'Hello, {} {}'.format (first_name + last_name)] 
+
+first_name = 'James'
+last_name = 'Peter'
+
+#Output 
+output = 'Hello ' + last_name + first_name
+print (output)
+
+
+#Output with curly brackets
+#The curly brackets assist with the order of variables. The variables number always starts from 0 going up
+output = 'Hello, {} {}'.format (first_name, last_name)
+output = 'Hello, {0} {1}'.format (first_name, last_name)
+output = f'Hello, {first_name}  {last_name}'
+
+print (output)
+
